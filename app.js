83cd1732ca98e13be7efe79c57efd6de722c4793
melinabed <141087878+melinabed@users.noreply.github.com>
2025-04-22 10:27:51 -1000

    added a simple route that renders a simple homepage

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 require("dotenv").config();
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const indexRouter = require("./routes/indexRouter");
+
+app.use("/", indexRouter);
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 const PORT = process.env.LOCAL_PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
---
+++ routes/indexRouter.js
@@ -0,0 +1,6 @@
+const { Router } = require("express");
+const indexRouter = Router();
+
+indexRouter.get("/", (req, res) => res.render("index"));
+
+module.exports = indexRouter;
