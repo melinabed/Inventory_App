c726d59ade3b01d391f8cb517a604ac5ae5099ed
melinabed <141087878+melinabed@users.noreply.github.com>
2025-04-22 15:16:46 -1000

    added images to db and new images folder to be displayed in ui

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -2,8 +2,7 @@ const db = require("../db/queries");
 
 async function getGames(req, res) {
   const games = await db.getAllGames();
-  console.log("Games: ", games);
-  res.send("Games: " + games.map((game) => game.name).join(", "));
+  res.render("index", { games: games });
 }
 
 module.exports = {
