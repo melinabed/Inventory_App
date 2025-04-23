98c3d0976e7492992be645119bc997b69ebebe09
melinabed <141087878+melinabed@users.noreply.github.com>
2025-04-23 10:36:22 -1000

    added error handling

--- app.js
+++ app.js
@@ -13,12 +13,23 @@ app.use("/", indexRouter);
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).render("./partials/404");
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render("./partials/error", { error: err });
+});
+
 const PORT = process.env.LOCAL_PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
-//A route shows all games (a new page);
 //Categories page shows pages by subgenre, platform, and game mode
 //Submit a game brings up a form to add a new game with heavy contraints
 //Search allows the page to display a game that was searched for
 //Maybe a delete game feature with secret password
 //Unique page with more details about game
+//Error Handling?
--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,14 +1,27 @@
 const db = require("../db/queries");
+const asyncHandler = require("express-async-handler");
 
-async function getGames(req, res) {
+const getGames = asyncHandler(async (req, res) => {
   const games = await db.getFeaturedGames();
-  res.render("index", { games: games });
-}
 
-async function getAllGames(req, res) {
+  if (!games || games.length === 0) {
+    res.status(404).send("No games found");
+    return;
+  }
+
+  res.render("index", { games: games });
+});
+
+const getAllGames = asyncHandler(async (req, res) => {
   const games = await db.getAllGames();
+
+  if (!games || games.length === 0) {
+    res.status(404).send("No games found");
+    return;
+  }
+
   res.render("allGames", { games: games });
-}
+});
 
 module.exports = {
   getGames,
