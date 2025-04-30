1c84d4cab897ffcf074a93186cb385ef481c89f0
melinabed <141087878+melinabed@users.noreply.github.com>
2025-04-30 13:32:07 -1000

    added mobile responsiveness

---
+++ public/script.js
@@ -0,0 +1,16 @@
+const sidebarBtnOpen = document.querySelector("#sidebar-button-open");
+const sidebarBtnClosed = document.querySelector("#sidebar-button-closed");
+
+const sidebar = document.querySelector(".sidebar");
+
+sidebarBtnOpen.addEventListener("click", () => {
+  sidebar.classList.toggle("sidebar-open");
+  sidebar.classList.toggle("sidebar-closed");
+  sidebarBtnOpen.style.display = "none";
+});
+
+sidebarBtnClosed.addEventListener("click", () => {
+  sidebar.classList.toggle("side-bar-closed");
+  sidebar.classList.toggle("sidebar-open");
+  sidebarBtnOpen.style.display = "block";
+});
