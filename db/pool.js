2ff67573fdd44c23e71ecac002620fe45b536556
melinabed <141087878+melinabed@users.noreply.github.com>
2025-05-01 12:42:02 -1000

    database update

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,8 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
