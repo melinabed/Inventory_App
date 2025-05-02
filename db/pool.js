bc6764dbc48a4015deed0e39e6709b80f1076e64
melinabed <141087878+melinabed@users.noreply.github.com>
2025-05-01 14:03:37 -1000

    another "fix"

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,11 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-module.exports = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
+
+module.exports = pool;
