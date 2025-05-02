698da87cde4928745d03beb08b5ae375e3ea332c
melinabed <141087878+melinabed@users.noreply.github.com>
2025-05-01 15:38:13 -1000

    change

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
